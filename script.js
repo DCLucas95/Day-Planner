c9a6588b8958c24f7394465661dbc8c0ce3e140f
DCLucas95 <69066157+DCLucas95@users.noreply.github.com>
2020-09-13 16:22:59 +1000

    refactored event listeners on save buttons

--- script.js
+++ script.js
@@ -19,78 +19,28 @@ var planning
 
 //if past current time, append class past
 function theTime(){
-    if timeOfDay; time is in the past.....Type-Something-Here.addClass("past")
+    timeOfDay > actualTime then .addClass - past
 }
 
 //if current time, append class present
 }
     else if timeOfDay;  {
-     time is in the present.....Type-Something-Here.addClass("past")
+     timeOfDay = actualTime then .addClass - present
 }
 
 //if after current time, append class future
     else if timeOfDay; {
-     time is in the future.....Type-Something-Here.addClass("past") 
+     timeOfDay < actualTime then .addClass - future 
 }
 
 */
 
 //keep entered text in local storage if save btn pressed
-var saveBtn1 = document.querySelector(".hour1")
-saveBtn1.addEventListener("click", makePlan);
-function makePlan(){
+
+$(".saveBtn").on("click", function() {
     console.log("details have been saved")
-}
-
-var saveBtn2 = document.querySelector(".hour2")
-saveBtn2.addEventListener("click", makePlan);
-function makePlan(){
-    console.log("details have been saved")
-}
-
-var saveBtn3 = document.querySelector(".hour3")
-saveBtn3.addEventListener("click", makePlan);
-function makePlan(){
-    console.log("details have been saved")
-}
-
-var saveBtn4 = document.querySelector(".hour4")
-saveBtn4.addEventListener("click", makePlan);
-function makePlan(){
-    console.log("details have been saved")
-}
-
-var saveBtn5 = document.querySelector(".hour5")
-saveBtn5.addEventListener("click", makePlan);
-function makePlan(){
-    console.log("details have been saved")
-}
-
-var saveBtn6 = document.querySelector(".hour6")
-saveBtn6.addEventListener("click", makePlan);
-function makePlan(){
-    console.log("details have been saved")
-}
-
-var saveBtn7 = document.querySelector(".hour7")
-saveBtn7.addEventListener("click", makePlan);
-function makePlan(){
-    console.log("details have been saved")
-}
-
-var saveBtn8 = document.querySelector(".hour8")
-saveBtn8.addEventListener("click", makePlan);
-function makePlan(){
-    console.log("details have been saved")
-}
-
-var saveBtn9 = document.querySelector(".hour9")
-saveBtn9.addEventListener("click", makePlan);
-function makePlan(){
-    console.log("details have been saved")
-}
-
-
+    //save text to local storage
+  });
 
 //button to clear local storage
 var clearDay = document.querySelector(".clearDay")
