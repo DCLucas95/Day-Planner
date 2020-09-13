1c807bf7d5c0614a3083bbaf254d6ca4698deca0
DCLucas95 <69066157+DCLucas95@users.noreply.github.com>
2020-09-13 22:38:36 +1000

    added local storage, need to work out how to retrieve. Also need to fix date being displayed twice

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
+//initialize the page after the page is loaded (document is reaady)
+$(document).ready(init);
+
 //global variables
 var timeOfDay = moment().format('MMMM Do YYYY, h:mm a')
-var planning
+var schedule = [];
 
+
+// function that initializes the page
+function init() {
+  console.log('page is ready');
 
 //display current date and time
-  $(document).ready (function(){
-      displayDate()
-  });
+      displayDate();
 
   function displayDate(){
     $('#timeofDay').append(timeOfDay);
@@ -34,20 +39,52 @@ function theTime(){
 }
 
 */
+// attach click event on all save buttons buttons on the page
+$('.saveBtn').on('click', saveButtonClick);
 
-//keep entered text in local storage if save btn pressed
+// read schedule from local storage and display it
+//displaySchedule();
+}
 
-$(".saveBtn").on("click", function() {
-    console.log("details have been saved")
-    //save text to local storage
-  });
+//function executed everytime a save button is pressed:\
+function saveButtonClick() {
+var hourSelected = parseInt($(this).attr('data-hour'));
+
+var description = $('#hour-' + hourSelected + ' .description')
+  .val()
+  .trim();
+  
+  schedule[hourSelected] = description;
+  
+//you can save only strings in the local storage
+var stringSchedule = JSON.stringify(schedule);
+
+localStorage.setItem('myDay', stringSchedule);
+}
+
+// read schedule from local storage and display it
+function displaySchedule() {
+var stringSchedule = localStorage.getItem('myDay');
+
+//convert string back to array
+schedule = JSON.parse(stringSchedule);
+
+console.log('stored schedule is:', schedule);
+
+// read every line in the schedule that was on the local
+// storage and set the schedule timetable on the screen accordingly
+for (var hour = 0; hour < schedule.length; hour++) {
+  console.log(schedule[hour]);
+
+  $('#hour-' + hour + ' .description').val(schedule[hour]);
+}
+}
 
 //button to clear local storage
-var clearDay = document.querySelector(".clearDay")
-
-clearDay.addEventListener("click", clearLocalStorage);
+$('.clearDay').on('click', clearLocalStorage);
 
 function clearLocalStorage(){
+    location.reload()
     window.localStorage.clear();
     alert("day has been cleared")
 }
